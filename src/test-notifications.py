1698d81dacaad7b539e84f0f7689e9982ba03980
jpetersen <jpetersen@7feff2eb-67fc-0310-aff0-fef983b50a43>
2009-01-20 13:28:11 +0000

    * src/notification-groups.conf: Remove D-Bus-Call keys, so the 	ActionInvoked signal is sent by default. Fixes NB#97103 	* src/test-notifications.py (emit_notification, 	  notification_closed_handler, action_invoked_handler): Update 	  test code. 	* configure.ac: 0.3.7 	* debian/changelog: Update
    git-svn-id: https://projects.maemo.org/svn/af/packages/fremantle/trunk/hildon-home@34201 7feff2eb-67fc-0310-aff0-fef983b50a43

--- src/test-notifications.py
+++ src/test-notifications.py
@@ -1,13 +1,36 @@
 #!/usr/bin/python2.5
 
-import dbus
 import time
 
-bus = dbus.SessionBus()
+import gobject
 
-proxy = bus.get_object('org.freedesktop.Notifications', '/org/freedesktop/Notifications')
+import dbus
+import dbus.mainloop.glib
 
-iface = dbus.Interface(proxy, 'org.freedesktop.Notifications')
+def emit_notification(iface):
+	id = iface.Notify ('test-send.py', '0', 'qgn_list_messagin', 'Jan Arne Petersen', 'Subject 1', ['default', 'default'], { 'category': 'email-message', 'time': dbus.Int64 (time.time() - 1800) }, 0)
+	print 'notification emmited. id: ' + str(id)
+
+	return True
+
+def notification_closed_handler(id):
+	print 'notification closed. id: ' + str(id)
+
+def action_invoked_handler(id, action_key):
+	print 'action invoked. id: ' + str(id) + ', action_key: ' + action_key
+
+if __name__ == '__main__':
+	dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+
+	bus = dbus.SessionBus()
+
+	try:
+		proxy = bus.get_object('org.freedesktop.Notifications', '/org/freedesktop/Notifications')
+		iface = dbus.Interface(proxy, 'org.freedesktop.Notifications')
+		proxy.connect_to_signal('NotificationClosed', notification_closed_handler, dbus_interface='org.freedesktop.Notifications')
+		proxy.connect_to_signal('ActionInvoked', action_invoked_handler, dbus_interface='org.freedesktop.Notifications')
+	except dbus.DBusException:
+		traceback.print_exc()
 
 # iface.SystemNoteInfoprint ('foo1');
 # iface.SystemNoteInfoprint ('foo2');
@@ -15,7 +38,7 @@ iface = dbus.Interface(proxy, 'org.freedesktop.Notifications')
 
 # iface.Notify ('test-send.py', '0', 'qgn_list_messagin', 'Jan Arne Petersen', 'Subject', ['default', 'default'], { 'category': 'email' }, 0)
 
-iface.Notify ('test-send.py', '0', 'qgn_list_messagin', 'Jan Arne Petersen', 'Subject 1', ['default', 'default'], { 'category': 'email-message', 'time': dbus.Int64 (time.time() - 1800) }, 0)
+	gobject.timeout_add(10000, emit_notification, iface)
 
-iface.Notify ('test-send.py', '0', 'qgn_list_messagin', 'Jan Arne Petersen', 'Subject 2', ['default', 'default'], { 'category': 'email-message', 'time': dbus.Int64 (time.time()) }, 0)
-
+	loop = gobject.MainLoop()
+	loop.run()
